6b31fe590c3b34f72b71b832a8f8e3952f3f2a56
vega jafri <giantwhitedeer@gmail.com>
2025-10-23 00:34:13 -0400

    basic setup

---
+++ escapeHalloween-2025/Assets/Scenes/scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Sprite[] playerSprites = new Sprite[4];
+    private SpriteRenderer currentSprite;
+    private Transform tr;
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSprite = GetComponent<SpriteRenderer>();
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxisRaw("Horizontal");
+        float zAxis = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = new Vector3(xAxis, 0f, zAxis);
+
+        tr.Translate(move * speed * Time.deltaTime, Space.World);
+        if (Input.GetKeyDown("w") || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            
+            currentSprite.sprite = playerSprites[0];
+        }
+
+        if (Input.GetKeyDown("a") || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            
+            currentSprite.sprite = playerSprites[1];
+        }
+
+        if (Input.GetKeyDown("s") || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            
+            currentSprite.sprite = playerSprites[2];
+        }
+
+        if (Input.GetKeyDown("d") || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            
+            currentSprite.sprite = playerSprites[3];
+        }
+    }
+}
